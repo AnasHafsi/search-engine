49abe8612abfa3e4433fdee56e84ff44a7dc2ed1
Anas <anashafsii@gmail.com>
2020-03-08 16:40:52 +0100

    Adding BatchSearch

---
+++ src/MyAnalyzer.java
@@ -0,0 +1,15 @@
+//import org.apache.lucene.analysis.PorterStemFilter;
+import org.apache.lucene.analysis.StopFilter;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.en.PorterStemFilter;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+//import org.apache.lucene.analysis.tokenattributes.TermAttribute;
+import org.apache.lucene.util.Version;
+
+import java.io.StringReader;
+
+public class MyAnalyzer {
+
+}
